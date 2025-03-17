1d06718edf0e358736320a88a802693579b10d77
suchita12900 <22BCS12900@cuchd.in>
2025-03-17 12:49:57 +0530

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,72 @@
+/*
+public class Assignment2{
+    public static void main(String[]args){
+        int x=8;
+        int a=x%10;
+        System.out.println("Unit digit of "+x+" is "+a);
+
+    }
+}
+
+
+public class Assignment2 {
+    public static void main(String[]args){
+        int x=453;
+        int a=x/10;
+        System.out.println("answer is "+a);
+    }
+}
+
+
+public class Assignment2 {
+    public static void main(String[]args)
+    {
+        int a=19;
+        int b=4;
+        System.out.println("a="+a+" b="+b);
+        int t;
+        t=a;
+        a=b;
+        b=t;
+        System.out.println("a="+a+" b="+b);
+
+    }
+}
+
+
+
+public class Assignment2 {
+    public static void main(String[]args)
+    {
+        int a=19;
+        int b=87;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("a="+a+" b="+b);
+
+    }
+}
+
+ *
+
+public class Assignment2 {
+    public static void main(String[]args)
+    {
+        int a=76^108;
+        System.out.println("a="+a);
+
+    }
+}
+
+ */
+
+public class Assignment2 {
+    public static void main(String[]args)
+    {
+        int x=2546,a;
+        a=x%10;
+        x=x/10;
+        System.out.println(a+""+x);
+    }
+}
